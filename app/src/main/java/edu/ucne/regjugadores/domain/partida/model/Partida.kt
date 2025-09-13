126b69ebcd8ecb92c91db56040438cd8bc7326dd
Esthiber <esthiber1@gmail.com>
2025-09-12 23:38:06 -0400

    Se Creo modelo de partida

---
+++ app/src/main/java/edu/ucne/regjugadores/domain/partida/model/Partida.kt
@@ -0,0 +1,12 @@
+package edu.ucne.regjugadores.domain.partida.model
+
+import java.util.Date
+
+data class Partida(
+    val partidaId: Int = 0,
+    val fecha: Date = Date(),
+    val jugador1ID: Int = 0,
+    val jugador2ID: Int = 0,
+    val ganadorID: Int? = null,
+    val esFinalizada: Boolean = false,
+)
