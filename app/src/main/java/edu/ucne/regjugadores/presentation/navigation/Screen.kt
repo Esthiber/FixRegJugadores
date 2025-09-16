6776bb360f549560516a2f3f07d22149eb746c4f
Esthiber <esthiber1@gmail.com>
2025-09-16 02:38:31 -0400

    Se creo Screen.kt

---
+++ app/src/main/java/edu/ucne/regjugadores/presentation/navigation/Screen.kt
@@ -0,0 +1,13 @@
+package edu.ucne.regjugadores.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed class Screen
+{
+    @Serializable
+    data object Jugadores:Screen()
+
+    @Serializable
+    data object Partidas:Screen()
+
+}
