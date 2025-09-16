28b6fb5ec15bd9668d0b2af4246fb1fba47f0656
Esthiber <esthiber1@gmail.com>
2025-09-16 02:48:19 -0400

    Se implemento NavHost

--- app/src/main/java/edu/ucne/regjugadores/MainActivity.kt
+++ app/src/main/java/edu/ucne/regjugadores/MainActivity.kt
@@ -16,12 +16,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.regjugadores.presentation.jugador.edit.EditJugadorScreen
 import edu.ucne.regjugadores.presentation.jugador.edit.EditJugadorUiEvent
 import edu.ucne.regjugadores.presentation.jugador.edit.EditJugadorViewModel
 import edu.ucne.regjugadores.presentation.jugador.list.ListJugadorViewModel
 import edu.ucne.regjugadores.presentation.jugador.list.ListJugadoresScreen
+import edu.ucne.regjugadores.presentation.navigation.RegJugadoresNavHost
 import edu.ucne.regjugadores.presentation.partida.edit.EditPartidaScreen
 import edu.ucne.regjugadores.presentation.partida.edit.EditPartidaViewModel
 import edu.ucne.regjugadores.presentation.partida.list.ListPartidaScreen
@@ -35,49 +37,54 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RegJugadoresTheme {
-                val editPartidaViewModel: EditPartidaViewModel = hiltViewModel()
-                val listPartidaViewModel: ListPartidaViewModel = hiltViewModel()
-
-                val editViewModel: EditJugadorViewModel = hiltViewModel()
-                val listViewModel: ListJugadorViewModel = hiltViewModel()
-                val listState by listViewModel.state.collectAsState()
-
-                LaunchedEffect(listState.navigateToEditId) {
-                    listState.navigateToEditId?.let { id ->
-                        editViewModel.onEvent(EditJugadorUiEvent.Load(id))
-                        listViewModel.onNavigationHandled()
-                    }
-                }
-
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                    ){
-                        EditPartidaScreen(viewModel = editPartidaViewModel)
-                        ListPartidaScreen(viewModel = listPartidaViewModel)
-//                        EditJugadorScreen(viewModel = editViewModel)
-//                        ListJugadoresScreen(viewModel = listViewModel)
-                    }
-                }
+                val navHost = rememberNavController()
+                RegJugadoresNavHost(navHost)
             }
+
+//            RegJugadoresTheme {
+//                val editPartidaViewModel: EditPartidaViewModel = hiltViewModel()
+//                val listPartidaViewModel: ListPartidaViewModel = hiltViewModel()
+//
+//                val editJugadorViewModel: EditJugadorViewModel = hiltViewModel()
+//                val listJugadorViewModel: ListJugadorViewModel = hiltViewModel()
+//                val listState by listJugadorViewModel.state.collectAsState()
+//
+//                LaunchedEffect(listState.navigateToEditId) {
+//                    listState.navigateToEditId?.let { id ->
+//                        editJugadorViewModel.onEvent(EditJugadorUiEvent.Load(id))
+//                        listJugadorViewModel.onNavigationHandled()
+//                    }
+//                }
+//
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Column(
+//                        modifier = Modifier
+//                            .fillMaxSize()
+//                            .padding(innerPadding)
+//                    ){
+//                        EditPartidaScreen(viewModel = editPartidaViewModel)
+//                        ListPartidaScreen(viewModel = listPartidaViewModel)
+////                        EditJugadorScreen(viewModel = editJugadorViewModel)
+////                        ListJugadoresScreen(viewModel = listJugadorViewModel)
+//                    }
+//                }
+//            }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun MainActivityPreview() {
     RegJugadoresTheme {
-        Greeting("Android")
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+                MainActivity()
+            }
+        }
     }
 }
