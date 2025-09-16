4ed54fe361f5df9cf2f520fed47ba626580a937d
Esthiber <esthiber1@gmail.com>
2025-09-16 02:47:02 -0400

    Se creo PartidaScreen.kt

---
+++ app/src/main/java/edu/ucne/regjugadores/presentation/partida/PartidaScreen.kt
@@ -0,0 +1,55 @@
+package edu.ucne.regjugadores.presentation.partida
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import edu.ucne.regjugadores.presentation.components.TopBarComponent
+import edu.ucne.regjugadores.presentation.jugador.JugadoresScreenBody
+import edu.ucne.regjugadores.presentation.partida.edit.EditPartidaScreen
+import edu.ucne.regjugadores.presentation.partida.edit.EditPartidaViewModel
+import edu.ucne.regjugadores.presentation.partida.list.ListPartidaScreen
+import edu.ucne.regjugadores.presentation.partida.list.ListPartidaViewModel
+import edu.ucne.regjugadores.ui.theme.RegJugadoresTheme
+
+@Composable
+fun PartidaScreen(
+    onDrawer: () -> Unit = {},
+    editViewModel: EditPartidaViewModel = hiltViewModel(),
+    listViewModel: ListPartidaViewModel = hiltViewModel()
+) {
+    Scaffold(
+        topBar = {
+            TopBarComponent(
+                title = "Registro de Partidas",
+                onDrawer
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            PartidaScreenBody(editViewModel, listViewModel)
+        }
+    }
+}
+
+@Composable
+fun PartidaScreenBody(
+    edit: EditPartidaViewModel = hiltViewModel(),
+    list: ListPartidaViewModel = hiltViewModel()
+) {
+    EditPartidaScreen(edit)
+    ListPartidaScreen(list)
+}
+
+@Preview
+@Composable
+fun PartidaScreenPreview() {
+    RegJugadoresTheme {
+        PartidaScreenBody()
+    }
+}
