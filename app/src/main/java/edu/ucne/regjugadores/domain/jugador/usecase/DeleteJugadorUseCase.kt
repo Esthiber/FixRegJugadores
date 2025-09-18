b5784dc28cf34410e160166f949dd2147c618e9e
Esthiber <esthiber1@gmail.com>
2025-09-17 20:01:05 -0400

    Se creo Actualizo DeleteJugadorUseCase.kt

--- app/src/main/java/edu/ucne/regjugadores/domain/jugador/usecase/DeleteJugadorUseCase.kt
+++ app/src/main/java/edu/ucne/regjugadores/domain/jugador/usecase/DeleteJugadorUseCase.kt
@@ -4,7 +4,25 @@ import edu.ucne.regjugadores.domain.jugador.repository.JugadorRepository
 import javax.inject.Inject
 
 class DeleteJugadorUseCase @Inject constructor(
-    private val repository: JugadorRepository
+    private val repository: JugadorRepository,
+    private val canDeleteJugadorUseCase: CanDeleteJugadorUseCase
 ) {
-    suspend operator fun invoke(id:Int) = repository.deleteJugadorbyId(id)
+    sealed class DeleteResult {
+        object Success : DeleteResult()
+        object HasPartidas : DeleteResult()
+        data class Error(val message: String) : DeleteResult()
+    }
+
+    suspend operator fun invoke(id: Int): DeleteResult {
+        return try {
+            if (!canDeleteJugadorUseCase(id)) {
+                DeleteResult.HasPartidas
+            } else {
+                repository.deleteJugadorbyId(id)
+                DeleteResult.Success
+            }
+        } catch (e: Exception) {
+            DeleteResult.Error(e.message ?: "Error desconocido al eliminar jugador")
+        }
+    }
 }
