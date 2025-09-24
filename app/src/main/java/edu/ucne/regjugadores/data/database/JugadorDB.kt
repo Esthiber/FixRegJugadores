dd240f42b316dd9c0dbc31026e76a4d6a0269ecc
Esthiber <esthiber1@gmail.com>
2025-09-24 17:19:31 -0400

    Se agrego LogroEntity a JugadorDB.kt

--- app/src/main/java/edu/ucne/regjugadores/data/database/JugadorDB.kt
+++ app/src/main/java/edu/ucne/regjugadores/data/database/JugadorDB.kt
@@ -4,19 +4,24 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import edu.ucne.regjugadores.data.jugadores.local.JugadorDao
 import edu.ucne.regjugadores.data.jugadores.local.JugadorEntity
+import edu.ucne.regjugadores.data.logros.local.LogroDao
+import edu.ucne.regjugadores.data.logros.local.LogroEntity
 import edu.ucne.regjugadores.data.partidas.local.PartidaDao
 import edu.ucne.regjugadores.data.partidas.local.PartidaEntity
 
 @Database(
     entities = [
         JugadorEntity::class,
-        PartidaEntity::class
+        PartidaEntity::class,
+        LogroEntity::class
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 abstract class JugadorDB : RoomDatabase() {
     abstract fun jugadorDao(): JugadorDao
 
     abstract fun partidaDao(): PartidaDao
+
+    abstract fun logroDao(): LogroDao
 }
