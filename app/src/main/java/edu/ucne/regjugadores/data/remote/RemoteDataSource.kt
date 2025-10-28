46525e923ad1c03d134a134e1b24f891bd24ac69
Esthiber <esthiber1@gmail.com>
2025-10-28 01:56:28 -0400

    Se agregaron metodos de jugador a la API

--- app/src/main/java/edu/ucne/regjugadores/data/remote/RemoteDataSource.kt
+++ app/src/main/java/edu/ucne/regjugadores/data/remote/RemoteDataSource.kt
@@ -1,6 +1,8 @@
 package edu.ucne.regjugadores.data.remote
 
 import edu.ucne.regjugadores.data.remote.dto.movimientosDto
+import edu.ucne.regjugadores.data.remote.dto.request.jugadores.JugadorRequest
+import edu.ucne.regjugadores.data.remote.dto.response.jugadores.JugadorResponse
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
@@ -11,4 +13,45 @@ class RemoteDataSource @Inject constructor(
 
     suspend fun postMovimiento(movimiento: movimientosDto): Unit =
         api.postMovimiento(movimiento)
+
+
+    suspend fun createJugador(req: JugadorRequest): Resource<JugadorResponse> {
+        return try {
+            val response = api.createJugador(req)
+            if (response.isSuccessful) {
+                response.body()?.let { Resource.Success(it) }
+                    ?: Resource.Error("Respuesta vacia del servidor")
+            } else {
+                Resource.Error("HTTP ${response.code()}: ${response.message()}")
+            }
+        } catch (e: Exception) {
+            Resource.Error("Error de red: ${e.localizedMessage ?: "Ocurrio un error desconocido"}")
+        }
+    }
+
+    suspend fun updateJugador(id: Int, req: JugadorRequest): Resource<Unit> {
+        return try {
+            val response = api.updateJugador(id, req)
+            if (response.isSuccessful) {
+                Resource.Success(Unit)
+            } else {
+                Resource.Error("HTTP ${response.code()}: ${response.message()}")
+            }
+        } catch (e: Exception) {
+            Resource.Error("Error de red: ${e.localizedMessage ?: "Ocurrio un error desconocido"}")
+        }
+    }
+
+    suspend fun  deleteJugador(id: Int): Resource<Unit> {
+        return try {
+            val response = api.deleteJugador(id)
+            if (response.isSuccessful) {
+                Resource.Success(Unit)
+            } else {
+                Resource.Error("HTTP ${response.code()}: ${response.message()}")
+            }
+        } catch (e: Exception) {
+            Resource.Error("Error de red: ${e.localizedMessage ?: "Ocurrio un error desconocido"}")
+        }
+    }
 }
