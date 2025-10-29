490a8079d7b0899526e77d14e723c806d1abbd74
Esthiber <esthiber1@gmail.com>
2025-10-29 00:33:46 -0400

    Se se creo e implemento workerFactory y SyncWorker.kt

--- app/src/main/java/edu/ucne/regjugadores/RegJugadoresApp.kt
+++ app/src/main/java/edu/ucne/regjugadores/RegJugadoresApp.kt
@@ -1,7 +1,27 @@
 package edu.ucne.regjugadores
 
 import android.app.Application
+import android.content.Context
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import dagger.hilt.android.HiltAndroidApp
+import edu.ucne.regjugadores.data.remote.worker.SyncWorker
+import javax.inject.Inject
 
 @HiltAndroidApp
-class RegJugadoresApp : Application() {}
+class RegJugadoresApp() : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
+
+fun triggerSyncWorker(context: Context) {
+    val req = OneTimeWorkRequestBuilder<SyncWorker>().build()
+    WorkManager.getInstance(context).enqueue(req)
+}
--- app/src/main/java/edu/ucne/regjugadores/domain/jugador/usecase/TriggerSyncUseCase.kt
+++ app/src/main/java/edu/ucne/regjugadores/domain/jugador/usecase/TriggerSyncUseCase.kt
@@ -1,7 +1,10 @@
 package edu.ucne.regjugadores.domain.jugador.usecase
 
 import android.content.Context
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import dagger.hilt.android.qualifiers.ApplicationContext
+import edu.ucne.regjugadores.data.remote.worker.SyncWorker
 import javax.inject.Inject
 
 class TriggerSyncUseCase @Inject constructor(
