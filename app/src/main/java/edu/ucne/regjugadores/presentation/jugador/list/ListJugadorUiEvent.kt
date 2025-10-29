8e0d56ed769d56d3240457a753c27b70f2e587f2
Esthiber <esthiber1@gmail.com>
2025-10-29 00:29:43 -0400

    Se eliminaron componentes incompatibles con el nuevo modelo de jugador y estructura del programa.

--- app/src/main/java/edu/ucne/regjugadores/presentation/jugador/list/ListJugadorUiEvent.kt
+++
@@ -1,9 +0,0 @@
-package edu.ucne.regjugadores.presentation.jugador.list
-
-interface ListJugadorUiEvent {
-    data object Load : ListJugadorUiEvent
-    data class Delete(val id: Int) : ListJugadorUiEvent
-    data object CreateNew : ListJugadorUiEvent
-    data class Edit(val id: Int) : ListJugadorUiEvent
-    data class ShowMessage(val message: String?) : ListJugadorUiEvent
-}
--- app/src/main/java/edu/ucne/regjugadores/presentation/partida/list/ListPartidaUiEvent.kt
+++
@@ -1,9 +0,0 @@
-package edu.ucne.regjugadores.presentation.partida.list
-
-interface ListPartidaUiEvent {
-    data object Load: ListPartidaUiEvent
-    data class Delete(val id: Int): ListPartidaUiEvent
-    data object CreateNew: ListPartidaUiEvent
-    data class Edit(val id: Int): ListPartidaUiEvent
-    data class ShowMessage(val message: String?): ListPartidaUiEvent
-}
