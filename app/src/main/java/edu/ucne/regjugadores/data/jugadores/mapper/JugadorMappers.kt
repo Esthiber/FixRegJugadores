c0bf55624b1cc7d0a2f72308323c3e6ac25bb0a6
Esthiber <esthiber1@gmail.com>
2025-10-29 00:34:54 -0400

    Se corrigio el mapper de jugadores.

--- app/src/main/java/edu/ucne/regjugadores/data/jugadores/mapper/JugadorMappers.kt
+++ app/src/main/java/edu/ucne/regjugadores/data/jugadores/mapper/JugadorMappers.kt
@@ -4,13 +4,15 @@ import edu.ucne.regjugadores.data.jugadores.local.JugadorEntity
 import edu.ucne.regjugadores.domain.jugador.model.Jugador
 
 fun JugadorEntity.toDomain(): Jugador = Jugador(
-    JugadorId = jugadorId,
-    Nombres = nombres,
-    Partidas = partidas
+    id = id,
+    nombres = nombres,
+    email = email,
+    isPendingCreate = isPendingCreate
 )
 
 fun Jugador.toEntity(): JugadorEntity = JugadorEntity(
-    jugadorId = JugadorId,
-    nombres = Nombres,
-    partidas = Partidas
+    id = id,
+    nombres = nombres,
+    email = email,
+    isPendingCreate = isPendingCreate
 )
